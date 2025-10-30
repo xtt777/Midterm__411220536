0b3ba19aa83902faea39d2a25a32ea4b4bde8adc
郭芯彤 <xxtong28@gmail.com>
2025-10-30 11:50:08 +0800

    M4-1 <message>

--- Integration.py
+++ Integration.py
@@ -72,7 +72,7 @@ def delete_student():
     print ('Folllowing row is delete:', delete)
     conn.commit()
 
-botton_delete = tk.Button(root, text='delete', command=delete_student)
+botton_delete = tk.Button(root, text='Delete', command=delete_student)
 botton_delete.pack(pady=25)
 
 root.mainloop() #must be put to the end of programming code
